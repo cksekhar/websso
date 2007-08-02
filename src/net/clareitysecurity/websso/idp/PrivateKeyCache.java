9e9c1c8861830978ad8ba647e2e21b280f6543ec
paulhethmon <paulhethmon@82e2188c-2d65-11df-8a22-590e308e4e5b>
2007-08-02 12:38:21 +0000

    Add bouncycastle

--- src/net/clareitysecurity/websso/idp/PrivateKeyCache.java
+++ src/net/clareitysecurity/websso/idp/PrivateKeyCache.java
@@ -24,14 +24,84 @@
 
 package net.clareitysecurity.websso.idp;
 
+import org.opensaml.xml.util.Base64;
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
+
+import java.security.KeyFactory;
+import java.security.PrivateKey;
+import java.security.spec.X509EncodedKeySpec;
+import java.security.spec.*;
+import java.security.*;
+import java.security.interfaces.*;
+
 /**
+ * This class represents a private key used to sign SAML objects.
  *
  * @author Paul Hethmon
  */
 public class PrivateKeyCache {
   
-  /** Creates a new instance of PrivateKeyCache */
-  public PrivateKeyCache() {
+  private String privateKeyEncoded;
+  
+  /*
+   * Set the BASE64 encoded value of the private key.
+   * @param s The BASE64 encoded value.
+   */
+  public void setPrivateKeyEncoded(String s) {
+    privateKeyEncoded = s;
+  }
+  /*
+   * Get the BASE64 encoded value of the private key.
+   * @return The BASE64 encoded value.
+   */
+  public String getPrivateKeyEncoded() {
+    return privateKeyEncoded;
   }
   
+  /** Creates a new instance of PrivateKeyCache */
+  public PrivateKeyCache() {
+    privateKeyEncoded = null;
+    Security.addProvider(new BouncyCastleProvider());
+  }
+  
+  public PrivateKey getPrivateKey() {
+    RSAPrivateKey privateKey = null;
+    KeyFactory keyFactory;
+    PKCS8EncodedKeySpec privSpec;
+    byte [] binaryKey;
+    
+    try {
+      keyFactory = KeyFactory.getInstance("RSA");
+      // decode private key
+      binaryKey = Base64.decode( privateKeyEncoded );
+      privSpec = new PKCS8EncodedKeySpec(binaryKey);
+      privateKey = (RSAPrivateKey) keyFactory.generatePrivate(privSpec);
+      return privateKey;
+    } catch (NoSuchAlgorithmException nsae) {
+      System.out.append("NoSuchAlgorithmException");
+      nsae.printStackTrace();
+      return null;
+    } catch (InvalidKeySpecException ikse) {
+      System.out.append("InvalidKeySpecException");
+      ikse.printStackTrace();
+      return null;
+    }
+//    if (privateKey != null) return privateKey;
+    
+/*            
+    // Create the Java PrivateKey object
+    X509EncodedKeySpec x509EncodedKeySpec = new X509EncodedKeySpec( Base64.decode( privateKeyEncoded ) );
+    // First try RSA encoded
+    if (privateKey != null) return privateKey;
+    
+    try {
+      keyFactory = KeyFactory.getInstance("DSA");
+      privateKey = keyFactory.generatePrivate(x509EncodedKeySpec);
+    } catch (NoSuchAlgorithmException nsae) {
+      
+    } catch (InvalidKeySpecException ikse) {
+      
+    }
+*/
+  }
 }
