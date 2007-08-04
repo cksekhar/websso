4a54f7c9bc701f9f63f74eb71dfc37c85a0dfc5e
paulhethmon <paulhethmon@82e2188c-2d65-11df-8a22-590e308e4e5b>
2007-08-04 03:23:38 +0000

    Final refactoring for HTTP POST and redirect handlers.

--- ClareitySecuritySSO/trunk/src/net/clareitysecurity/websso/sp/RedirectHandler.java
+++ ClareitySecuritySSO/trunk/src/net/clareitysecurity/websso/sp/RedirectHandler.java
@@ -55,67 +55,11 @@ import org.w3c.dom.Element;
  *
  * @author Paul Hethmon
  */
-public class RedirectHandler extends AbstractHttpHandler  {
+public class RedirectHandler extends AbstractHttpHandler {
+
+  protected String
+    relayState;
   
-  private String
-      issuerName,
-      providerName,
-      actionURL,
-      assertionConsumerServiceURL,
-      relayState;
-  
-  /*
-   * The IssuerName is the unique identifier value of your application.
-   */
-  public void setIssuerName(String newIssuerName) {
-    issuerName = newIssuerName;
-  }
-  /*
-   * Get the current value of the IssuerName.
-   */
-  public String getIssuerName() {
-    return issuerName;
-  }
-  /*
-   * The ProviderName is the human readable name of your application for use
-   * by the Identity Provider.
-   */
-  public void setProviderName(String newProviderName) {
-    providerName = newProviderName;
-  }
-  /*
-   * Get the current value of the ProviderName.
-   */
-  public String getProviderName() {
-    return providerName;
-  }
-  /*
-   * The ActionURL is the fully formed URL where the SAML Request will be redirected
-   * to. It should be of the form <b>http://www.acmeidp.com/recv-authnrequest.jsp</b>.
-   */
-  public void setActionURL(String newActionURL) {
-    actionURL = newActionURL;
-  }
-  /*
-   * Get the current value of the ActionURL.
-   */
-  public String getActionURL() {
-    return actionURL;
-  }
-  /*
-   * The AssertionConsumerServiceURL is the URL location that the Idp will return
-   * the Assertion to once the user is authenticated. It should be a fully
-   * formed URL such as <b>http://www.acmemls.com/recv-saml.jsp</b>.
-   */
-  public void setAssertionConsumerServiceURL(String newAssertionConsumerServiceURL) {
-    assertionConsumerServiceURL = newAssertionConsumerServiceURL;
-  }
-  /*
-   * Get the current value of the AssertionConsumerServiceURL.
-   */
-  public String getAssertionConsumerServiceURL() {
-    return assertionConsumerServiceURL;
-  }
   public void setRelayState(String newRelayState) {
     relayState = newRelayState;
   }
@@ -127,8 +71,7 @@ public class RedirectHandler extends AbstractHttpHandler  {
    * Create the RedirectHandler object for SP usage.
    */
   public RedirectHandler() throws org.opensaml.xml.ConfigurationException {
-    // do the bootstrap thing and make sure the library is happy
-    org.opensaml.DefaultBootstrap.bootstrap();
+    super();
   }
   
   /*
@@ -140,34 +83,14 @@ public class RedirectHandler extends AbstractHttpHandler  {
   public void sendSAMLRedirect(HttpServletResponse response) throws org.opensaml.xml.io.MarshallingException, BindingException, IOException {
     String samlRequest;
     
-    // Use the OpenSAML Configuration singleton to get a builder factory object
-    XMLObjectBuilderFactory builderFactory = org.opensaml.Configuration.getBuilderFactory();
-    // First get a builder for AuthnRequest
-    AuthnRequestBuilder arb = (AuthnRequestBuilder) builderFactory.getBuilder(AuthnRequest.DEFAULT_ELEMENT_NAME);
-    // And one for Issuer
-    IssuerBuilder ib = (IssuerBuilder) builderFactory.getBuilder(Issuer.DEFAULT_ELEMENT_NAME);
-    // And one for Subject
-    SubjectBuilder sb = (SubjectBuilder) builderFactory.getBuilder(Subject.DEFAULT_ELEMENT_NAME);
     // build an AuthnRequest object
-    AuthnRequestImpl auth = (AuthnRequestImpl) arb.buildObject();
-    // Build the Issuer object
-    Issuer newIssuer = ib.buildObject();
-    newIssuer.setValue(issuerName);
-    auth.setIssuer(newIssuer);
-    auth.setProviderName(providerName);
-    auth.setAssertionConsumerServiceURL(assertionConsumerServiceURL);
-    auth.setDestination(actionURL);
-    //auth.setAssertionConsumerServiceIndex(0);
-    //auth.setAttributeConsumingServiceIndex(0);
-    auth.setVersion(org.opensaml.common.SAMLVersion.VERSION_20);
-    DateTime dt = new DateTime();
-    auth.setIssueInstant(dt);
-    auth.setID("acmemls:" + dt.getMillis());
+    AuthnRequestImpl auth = buildAuthnRequest();
 
+    // Now we must marshall the object for the transfer over the wire.
     Marshaller marshaller = org.opensaml.Configuration.getMarshallerFactory().getMarshaller(auth);
     Element authDOM = marshaller.marshall(auth);
     // We use a StringWriter to produce our XML output. This gets us XML where
-    // the encoding is UTF-8
+    // the encoding is UTF-8. We must have UTF-8 or bad things happen.
     StringWriter rspWrt = new StringWriter();
     XMLHelper.writeNode(authDOM, rspWrt);
     String messageXML = rspWrt.toString();
@@ -201,19 +124,6 @@ public class RedirectHandler extends AbstractHttpHandler  {
     deflater.finish();
     compressedLength = deflater.deflate(data);
     return ( Base64.encodeBytes(data, 0, compressedLength) );
-    
-      /*
-      ByteArrayOutputStream messageOut = new ByteArrayOutputStream();
-      Base64.OutputStream b64Out = new Base64.OutputStream(messageOut);
-      Deflater deflater = new Deflater(Deflater.DEFLATED, true);
-      DeflaterOutputStream deflaterStream = new DeflaterOutputStream(b64Out, deflater);
-      deflaterStream.write(messageStr.getBytes());
-      deflaterStream.close();
-      return messageOut.toByteArray();
-    } catch (IOException e) {
-      throw new BindingException("Unable to DEFLATE and Base64 encode SAML message", e);
-    }
-       **/
   }
   
   /**
