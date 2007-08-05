c383988b24920e5d9af454c9990670d7953059c1
paulhethmon <paulhethmon@82e2188c-2d65-11df-8a22-590e308e4e5b>
2007-08-05 17:45:00 +0000

    Add ProtocolBinding to AuthnRequest statements.

--- src/net/clareitysecurity/websso/sp/RedirectHandler.java
+++ src/net/clareitysecurity/websso/sp/RedirectHandler.java
@@ -116,6 +116,7 @@ public class RedirectHandler extends AbstractHttpHandler {
     
     // build an AuthnRequest object
     AuthnRequestImpl auth = buildAuthnRequest();
+    auth.setProtocolBinding( org.opensaml.saml2.binding.encoding.HTTPRedirectDeflateEncoder.BINDING_URI );
 
     // Now we must marshall the object for the transfer over the wire.
     Marshaller marshaller = org.opensaml.Configuration.getMarshallerFactory().getMarshaller(auth);
